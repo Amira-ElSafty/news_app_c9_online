f2098e28d77bf1e9b31675b72e35917902936fb9
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2023-10-07 22:47:56 +0300

    initial project

---
+++ android/app/src/main/kotlin/com/example/flutter_news_app_c9_online/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_news_app_c9_online
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app_c9_online/home/category/category_details.dart';
+import 'package:flutter_news_app_c9_online/home/home_screen.dart';
+import 'package:flutter_news_app_c9_online/my_theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        CategoryDetails.routeName: (context) => CategoryDetails(),
+      },
+      initialRoute: CategoryDetails.routeName,
+      theme: MyTheme.lightTheme,
+    );
+  }
+}
